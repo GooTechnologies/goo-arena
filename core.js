4d6a5cba3f933576f227ade74a01985fa5a3debd
Victor Sand <victor.sand@gmail.com>
2014-12-01 16:41:28 +0100

    major refactor WIP

--- core.js
+++ core.js
@@ -1,280 +1,411 @@
+'use strict';
 var Vector3 = require('./vector3.js');
 
-// The core constants. The ultimate truth.
 function GameCore() {
 
-  this.moveSpeed = 8;
-  this.turnSpeed = -0.001;
-  this.forward = new Vector3([0, 0, -1]);
-  this.left = new Vector3([-1, 0, 0]);
-  this.hitRadius = 1;
-  this.spawnLimit = 15;
-  this.aimHeight = 2;
-  this.startHealth = 30;
-  this.spawnTime = 5000;
+	this.constants = {
+		moveSpeed: 8,
+		turnSpeed: -0.001,
+		forward: new Vector3(0, 0, -1),
+		left: new Vector3(-1, 0, 0),
+		hitRadius: 1,
+		spawnLimit: 20,
+		aimHeight: 2,
+		spawnTime: 5000,
+		numOccluders: 10,
+		occluderRadiusMin: 4,
+		occluderRadiusMax: 12,
+		occluderSpawnLimit: 40,
+		walls: 50,
+		startHealth: 10
+	};
 
-  this.numOccluders = 10;
-  this.occluderRadiusMin = 4;
-  this.occluderRadiusMax = 12;
-  this.occluderSpawnLimit = 40;
+	this.kills = [];
+	this.shots = [];
+	this.hits = [];
+	this.spawnedPlayers = [];
 
-  this.walls = 50;
+	this.players = {};
+	this.playersOld = {};
 
-};
+	this.handledDeltas = {};
+	this.deltaQueues = {};
 
-GameCore.prototype.newPlayer = function(id, occluders) {
+	this.occluders = [];
+	this.generateOccluders();
 
-  var that = this;
-
-  var position;
-  var clear = false;
-  while (!clear) {
-    //console.log('Trying to find a clear spot');
-    clear = true;
-    position = new Vector3([this.getRandomArbitrary(-this.spawnLimit, this.spawnLimit), 0, this.getRandomArbitrary(-this.spawnLimit, this.spawnLimit)]);
-    occluders.forEach(function(v) {
-      var d = Vector3.sub(position, v.position);
-      if (d.mag() < that.hitRadius+v.radius+2) {
-        clear = false;
-      }
-    });
-  }
-
-  return {
-    id: id,
-    name: 'Goon #' + this.getRandomInt(1, 999),
-    kills: 0,
-    deaths: 0,
-    alive: false,
-    timeToSpawn: 4000, // Give the client some time to init and set name
-    position: position,
-    rotation: [0, 0],
-    latency: 150,
-    health: this.startHealth,
-    color: [this.getRandomArbitrary(0, 1),
-      this.getRandomArbitrary(0, 1), this.getRandomArbitrary(0, 1)]
-  };
-};
-
-GameCore.prototype.generateOccluders = function() {
-  var occluders = [];
-  for (var i=0; i<this.numOccluders; i++) {
-    var occluder = {
-      radius: this.getRandomArbitrary(this.occluderRadiusMin, this.occluderRadiusMax),
-      position: new Vector3([
-        this.getRandomArbitrary(-this.occluderSpawnLimit, this.occluderSpawnLimit), 
-        -2, 
-        this.getRandomArbitrary(-this.occluderSpawnLimit, this.occluderSpawnLimit)
-      ])
-    };
-    occluders.push(occluder);
-  } 
-  return occluders;
-};
-
-GameCore.prototype.spawnPlayer = function(player, occluders) {
-  var that = this;
-
-  var position;
-  var clear = false;
-  while (!clear) {
-    //console.log('Trying to find a clear spot');
-    clear = true;
-    position = new Vector3([this.getRandomArbitrary(-this.spawnLimit, this.spawnLimit), 0, this.getRandomArbitrary(-this.spawnLimit, this.spawnLimit)]);
-    occluders.forEach(function(v) {
-      var d = Vector3.sub(position, v.position);
-      if (d.mag() < that.hitRadius+v.radius+2) {
-        clear = false;
-      }
-    });
-  }
-
-  player.alive = true;
-  player.timeToSpawn = -1;
-  player.position =  position;
-  player.health = this.startHealth;
 }
 
-GameCore.prototype.killPlayer = function(player) {
-  player.alive = false;
-  player.timeToSpawn = this.spawnTime;
-  player.health = 0;
+
+GameCore.prototype.updatePlayers = function(tickLength) {
+	var p;
+	var that = this;
+
+	this.spawnedPlayers = [];
+	this.kills = [];
+	this.hits = [];
+	this.kills = [];
+
+	Object.keys(this.players).forEach(function(v) {
+		p = that.players[v];
+		if (p.alive === false) {
+			if (p.timeToSpawn > 0) {
+				p.timeToSpawn -= tickLength;
+			} else {
+				that.spawnPlayer(v);
+				that.spawnedPlayers.push(p);
+			}
+		} else {
+			that.applyDeltaQueue(v);
+		}
+	});
+
 };
 
-GameCore.prototype.applyDelta = function(player, delta, players, occluders) {
-  var p, d, that;
-  that = this;
+GameCore.prototype.setPlayerValue = function(id, key, value) {
+	this.players[id][key] = value;
+};
 
-  // Apply!
-  player.position.x += delta[0];
-  player.position.y += delta[1];
-  player.position.z += delta[2];
+GameCore.prototype.pushDelta = function(id, delta) {
+	this.deltaQueues[id].push(delta);
+};
 
-   // Simple wall collision
-  if (player.position.x+this.hitRadius > this.walls || player.position.x-this.hitRadius < -this.walls) {
-    //console.log('Wall collision');
-    player.position.x -= delta[0];
-  }
-  if (player.position.z+this.hitRadius > this.walls || player.position.z-this.hitRadius < -this.walls) {
-    //console.log('Wall collision');
-    player.position.z -= delta[2];
-  }
 
-  // Simple player collision
-  Object.keys(players).forEach(function(v) {
-    d = Vector3.sub(player.position, players[v].position);
-    if (v != player.id && d.mag() < that.hitRadius*2) {
-      //console.log('Player collision');
-      d.normalize();
-      d.scale(0.3);
-      d.y = 0;
-      player.position.add(d);
-    }
-  });
+GameCore.prototype.applyDeltaQueue = function(id) {
+	var that = this;
+	this.deltaQueues[id].forEach(function(v) {
+		that.applyDelta(id, v);
+		that.handledDeltas[id]++;
+	});
+	this.deltaQueues[id] = [];
+};
 
-  // Simple occluder collision
-  occluders.forEach(function(v) {
-    d = Vector3.sub(player.position, v.position);
-    if (d.mag() < that.hitRadius + v.radius) {
-      //console.log('Occluder collision');
-      d.normalize();
-      d.scale(0.3);
-      d.y = 0;
-      player.position.add(d);
-    }
-  });
+
+GameCore.prototype.updateInterpolationState = function() {
+	var that = this;
+	this.playersOld = {};
+	Object.keys(this.players).forEach(function(v) {
+		that.playersOld[v] = that.copyForInterpolation(v);
+	});
+};
+
+// Find a non-occluded player spot
+// TODO check for player-player collisions
+GameCore.prototype.freeSpot = function() {
+	var that = this;
+	var position, d;
+	var clear = false;
+	while (!clear) {
+		clear = true;
+		position = new Vector3(
+			this.getRandomArbitrary(-this.constants.spawnLimit, this.constants.spawnLimit),
+			0, 
+			this.getRandomArbitrary(-this.constants.spawnLimit, this.constants.spawnLimit)
+		);
+		this.occluders.forEach(function(v) {
+			d = Vector3.sub(position, v.position);
+			if (d.mag() < that.hitRadius + v.radius + 2) {
+				clear = false;
+			}
+		});	
+	}
+	return position;
+};
+
+
+GameCore.prototype.newPlayer = function(id) {
+	console.log('Creating new player', id);
+	this.players[id] = {
+		id: id,
+		name: 'Goon #' + this.getRandomInt(1, 999),
+		kills: 0,
+		deaths: 0,
+		alive: false,
+		timeToSpawn: 3000, // Give the client some time to init and set name
+		position: this.freeSpot(),
+		rotation: [0, 0],
+		latency: 50, // Guess
+		health: this.constants.startHealth,
+		color: [
+			this.getRandomArbitrary(0, 1),
+			this.getRandomArbitrary(0, 1), 
+			this.getRandomArbitrary(0, 1)
+		]
+	};
+	this.handledDeltas[id] = 0;
+	this.deltaQueues[id] = [];
+	return this.players[id];
+};
+
+
+GameCore.prototype.removePlayer = function(id) {
+	console.log('Removing player');
+	delete this.players[id];
+};
+
+
+GameCore.prototype.generateOccluders = function() {
+	console.log('Generating', this.constants.numOccluders, 'occluders');
+	for (var i = 0; i < this.constants.numOccluders; i++) {
+		var occluder = {
+			radius: this.getRandomArbitrary(this.constants.occluderRadiusMin, this.constants.occluderRadiusMax),
+			position: new Vector3(
+				this.getRandomArbitrary(-this.constants.occluderSpawnLimit, this.constants.occluderSpawnLimit), 
+				-2,
+				this.getRandomArbitrary(-this.constants.occluderSpawnLimit, this.constants.occluderSpawnLimit)
+			)
+		};
+		this.occluders.push(occluder);
+	}
+};
+
+
+GameCore.prototype.spawnPlayer = function(id) {
+	var player = this.players[id];
+	console.log('Spawning player', player.name, player.id);
+	player.position = this.freeSpot();
+	player.alive = true;
+	player.timeToSpawn = -1;
+	player.health = this.constants.startHealth;
+
+	console.log('Now there are', Object.keys(this.players).length, 'players');
+};
+
+GameCore.prototype.killPlayer = function(id) {
+	var player = this.players[id];
+	console.log('Killing player', player, id);
+	player.alive = false;
+	player.timeToSpawn = this.constants.spawnTime;
+	player.health = 0;
+};
+
+// Apply a movement delta, handle collisions
+// TODO validate the delta against some max allowed speed
+// to prevent modified clients to supply cheat deltas
+GameCore.prototype.applyDelta = function(id, delta) {
+	var d, that;
+	var player = this.players[id];
+	that = this;
+
+	// Apply!
+	player.position.x += delta[0];
+	player.position.y += delta[1];
+	player.position.z += delta[2];
+
+	// Simple wall collision, revert the delta causing the wall collision
+	if (player.position.x + this.constants.hitRadius > this.constants.walls ||
+		player.position.x - this.constants.hitRadius < -this.constants.walls) {
+		player.position.x -= delta[0];
+	}
+	if (player.position.z + this.constants.hitRadius > this.constants.walls ||
+		player.position.z - this.constants.hitRadius < -this.constants.walls) {
+		player.position.z -= delta[2];
+	}
+
+	// Player and occluder sphere-sphere intersection, move the player
+	// away if it collides.
+	// TODO nicer collision handling
+
+	// Simple player collision
+	Object.keys(this.players).forEach(function(v) {
+		d = Vector3.sub(player.position, that.players[v].position);
+		if (v != player.id && d.mag() < that.constants.hitRadius * 2) {
+			d.normalize();
+			d.scale(0.2);
+			d.y = 0;
+			player.position.add(d);
+		}
+	});
+
+	// Simple occluder collision
+	this.occluders.forEach(function(v) {
+		d = Vector3.sub(player.position, v.position);
+		if (d.mag() < that.constants.hitRadius + v.radius) {
+			d.normalize();
+			d.scale(0.2);
+			d.y = 0;
+			player.position.add(d);
+		}
+	});
+
+	//console.log('After delta', player.position);
 
 };
 
 // Do the hit interpolation and calculations
-GameCore.prototype.fire = function(players, state_old, update_time, average_tick_rate, latency, socket_id, source, direction, occluders) {
-  var r, e, t, d, c, A, B, C, emc, discSq, disc, t, t1, t2, target, point, playerT, sphereHit, target_id, that;
-  that = this;
-  interpolatedPlayers = this.interpolatePlayers(players, state_old, update_time, average_tick_rate, latency);
+// TODO validate source position - could the player reasonably fire from that spot?
+GameCore.prototype.fire = function(update_time, average_tick_rate, id, source, direction) {
+	var r, e, d, c, A, point, playerT, sphereHit, target_id, occluder, interpolatedPlayers;
+	var that = this;
 
-  // Hit data to return
-  point = null;
-  target_id = -1;
+	//console.log('Shot from', source, 'to', direction);
 
-  r = this.hitRadius;
-  d = new Vector3(direction);
-  d.normalize();
-  e = new Vector3(source);
-  e.add(new Vector3([0, this.aimHeight, 0]));
-  A = Vector3.dot(d, d);
+	this.shots.push({
+		shooter: id,
+		source: source
+	});
 
-  // Check against all (interpolated) player positions and hit sphere
+	// Hit data to return
+	point = null;
+	target_id = -1;
 
-  playerT = 99999;
+	if (this.players[id].alive === true) {
 
-  Object.keys(interpolatedPlayers).forEach(function(v) {
-    if (v != socket_id && players[v].health > 0) {
-      c = interpolatedPlayers[v].position.clone();
-      sphereHit = that.raySphereIntersect(A, e, d, c, r);
-      if (sphereHit !== null) {
-        if (sphereHit.t < playerT) {
-          //console.log('Hit a player! Maybe.');
-          playerT = sphereHit.t;
-          point = sphereHit.point;
-          target_id = v;
-        }
-      }
-    }
-  });
+		// Get the interpolated player we're shooting at, using the shooter's point of view (latency)
+		interpolatedPlayers = this.interpolatePlayers(update_time, average_tick_rate, this.players[id].latency);
 
-  // Did we hit any occluders closer to the closest player?
-  for (var i=0; i<occluders.length; i++) {
-    var occluder = occluders[i];
-    c = occluder.position.clone();
-    r = occluder.radius;
-    sphereHit = that.raySphereIntersect(A, e, d, c, r);
-    if (sphereHit !== null && sphereHit.t < playerT) {
-      //console.log('Occluder hit before player');
-      target_id = -1;
-      break;
-    }
-  }
+		// Radius of target sphere
+		r = this.constants.hitRadius;
+		// Direction of shot
+		d = new Vector3(direction[0], direction[1], direction[2]);
+		d.normalize();
+		// Origin of shot
+		e = new Vector3(source[0], source[1], source[2]);
+		e.add(new Vector3(0, this.constants.aimHeight, 0));
 
-  // Did we hit anything? If we did, remove some health on target
-	if (target_id > 0) {
-		players[target_id].health--;
-	} 
+		A = Vector3.dot(d, d);
 
-  // Send back the hit data
-  return { target_id: target_id, point: point };
+		// Check against all (interpolated) player positions and hit spheres
+		playerT = 99999;
+
+		Object.keys(interpolatedPlayers).forEach(function(v) {
+			//console.log('Checking against', that.players[v]);
+			if (v != id && that.players[v].health > 0) {
+				c = interpolatedPlayers[v].position.clone();
+				//console.log('At position', c);
+				sphereHit = that.raySphereIntersect(A, e, d, c, r);
+				if (sphereHit !== null) {
+					if (sphereHit.t < playerT) {
+						playerT = sphereHit.t;
+						point = sphereHit.point;
+						target_id = v;
+					}
+				}
+			}
+		});
+
+		// Did we hit any occluders closer to the closest player?
+		for (var i=0; i<this.occluders.length; i++) {
+			occluder = this.occluders[i];
+			c = occluder.position.clone();
+			r = occluder.radius;
+			sphereHit = that.raySphereIntersect(A, e, d, c, r);
+			if (sphereHit !== null && sphereHit.t < playerT) {
+				target_id = -1;
+				break;
+			}
+		}
+
+		// Did we hit anything? If we did, remove some health on target
+		if (target_id > -1) {
+			console.log(this.players[id].name, 'shot', this.players[target_id].name, 'at', point);
+			this.players[target_id].health--;
+			this.hits.push({
+				shooter: id,
+				victim: target_id,
+				point: point
+			});
+			// Did we kill it?
+			if (this.players[target_id].health <= 0) {
+				this.players[target_id].deaths++;
+				this.players[id].kills++;
+				this.kills.push({
+					shooter: id,
+					victim: target_id
+				});
+				this.killPlayer(target_id);
+			}
+		} else {
+			console.log('Miss');
+		}
+
+	} else {
+		console.log('You can\'t shoot if you\'re dead!');
+	}
+
+
+	// Send back the hit data
+	return {
+		target_id: target_id,
+		point: point
+	};
 };
 
 // == Intersect =========================================================
 
 GameCore.prototype.raySphereIntersect = function(A, e, d, c, r) {
-  var emc, B, C, discSq, t1, t2, t, point;
-  emc = Vector3.sub(e, c);
-  B = Vector3.dot(Vector3.scale(d, 2), emc);
-  C = Vector3.dot(emc, emc) - r*r;
-  discSq = B*B - 4*A*C;
-  if (discSq >= 0) {
-    disc = Math.sqrt(discSq);
-    t1 = (disc-B)/(2*A);
-    t2 = (-disc-B)/(2*A);
-    t = (t1 < t2) ? t1 : t2;
-    if (t > 0) {
-      point = Vector3.add(e, (Vector3.scale(d, t))).toArray();
-      return { t: t, point: point };
-    }
-  }
-  return null;
+
+	var emc, B, C, discSq, t1, t2, t, point, disc;
+	emc = Vector3.sub(e, c);
+	B = Vector3.dot(Vector3.scale(d, 2), emc);
+	C = Vector3.dot(emc, emc) - r * r;
+	discSq = B * B - 4 * A * C;
+	if (discSq >= 0) {
+		disc = Math.sqrt(discSq);
+		t1 = (disc - B) / (2 * A);
+		t2 = (-disc - B) / (2 * A);
+		t = (t1 < t2) ? t1 : t2;
+		if (t > 0) {
+			point = Vector3.add(e, (Vector3.scale(d, t))).toArray();
+			return {
+				t: t,
+				point: point
+			};
+		}
+	}
+	return null;
 };
 
 // == Random  ===========================================================
 
 GameCore.prototype.getRandomInt = function(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+	return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
 GameCore.prototype.getRandomArbitrary = function(min, max) {
-  return Math.random() * (max - min) + min;
+	return Math.random() * (max - min) + min;
 };
 
 // == Interpolation  ====================================================
 
 // Copy what's needed for interpolation
-GameCore.prototype.copyForInterpolation = function(player) {
-  return {
-    position: player.position.clone(),
-    rotation: player.rotation.slice()
-  }
+GameCore.prototype.copyForInterpolation = function(id) {
+	return {
+		position: this.players[id].position.clone(),
+		rotation: this.players[id].rotation.slice()
+	};
 };
 
-GameCore.prototype.interpolatePlayers = function(players, state_old, update_time, average_tick_rate, latency) {
-  var that = this;
-  var t = (new Date().getTime()-update_time-latency)/average_tick_rate;
-  var interpolatedPlayers = {};
-  Object.keys(players).forEach(function(v) {
-    if (state_old[v]) {
-      interpolatedPlayers[v] = that.interpolatePlayer(players[v], state_old[v], t);
-    } else {
-      interpolatedPlayers[v] = that.copyForInterpolation(players[v]);
-    }
-  });
-  return interpolatedPlayers;
+GameCore.prototype.interpolatePlayers = function(update_time, average_tick_rate, latency) {
+	var that = this;
+	var t = (new Date().getTime() - update_time - latency) / average_tick_rate;
+	var interpolatedPlayers = {};
+	Object.keys(this.players).forEach(function(v) {
+		if (that.playersOld[v]) {
+			interpolatedPlayers[v] = that.interpolatePlayer(that.players[v], that.playersOld[v], t);
+		} else {
+			interpolatedPlayers[v] = that.copyForInterpolation(v);
+		}
+	});
+	return interpolatedPlayers;
 };
 
 GameCore.prototype.interpolatePlayer = function(playerOld, playerOlder, t) {
-  return {
-    position: new Vector3([
-      this.lerp(playerOlder.position.x, playerOld.position.x, t),
-      this.lerp(playerOlder.position.y, playerOld.position.y, t),
-      this.lerp(playerOlder.position.z, playerOld.position.z, t),
-    ]),
-    rotation: [
-      this.lerp(playerOlder.rotation[0], playerOlder.rotation[1], t),
-      this.lerp(playerOlder.rotation[1], playerOlder.rotation[1], t)
-    ]
-  };
+	return {
+		position: new Vector3(
+			this.lerp(playerOlder.position.x, playerOld.position.x, t),
+			this.lerp(playerOlder.position.y, playerOld.position.y, t),
+			this.lerp(playerOlder.position.z, playerOld.position.z, t)
+		),
+		rotation: [
+			this.lerp(playerOlder.rotation[0], playerOlder.rotation[1], t),
+			this.lerp(playerOlder.rotation[1], playerOlder.rotation[1], t)
+		]
+	};
 };
 
 GameCore.prototype.lerp = function(a, b, t) {
-  return a*(1-t) + b*t;
+	return a * (1 - t) + b * t;
 };
 
 module.exports = GameCore;
--- server.js
+++ server.js
@@ -1,82 +1,57 @@
-var WebSocketServer = require('ws').Server
-var http = require('http')
-var express = require('express')
-var app = express()
-var port = process.env.PORT || 5000
+'use strict';
+var WebSocketServer = require('ws').Server;
+var http = require('http');
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 5000;
 
 var GameCore = require('./core.js');
 var core = new GameCore();
 console.log('Loaded core');
 
-app.use(express.static(__dirname + "/"))
+app.use(express.static(__dirname + "/"));
 
 var server = http.createServer(app);
 server.listen(port);
 console.log('Listening on port', port);
 
 var wss = new WebSocketServer({server: server});
-console.log('Server created')
+console.log('Server created');
 
 // Stores connections
+// With the same IDs as players
 var sockets = {};
-
-// Stuff we can't shoot
-var occluders = core.generateOccluders();
-console.log('Occluders', occluders);
-
-// Player state (same ID as sockets)
-var players = {};
-
-// Previous player state (only what's neccessary for interpolation)
-var state_old = {};
-
-// There could be several of these per udpate frame, no need to send instantly
-var shots = [];
-var hits = [];
-var kills = [];
 
 // Keep track of average tick length for interpolation when shot is fired
 var recent_ticks = [tick_rate, tick_rate, tick_rate, tick_rate, tick_rate];
 var average_tick_rate = tick_rate;
 
-// Inputs from the client
-var handled_deltas = {};
-var delta_queues = {};
-
 // Some server state 
 var socket_id_counter = 0;
-var num_connections = 0;
-var server_time = 0;
 var tick_rate = 50;
 var tick_length = tick_rate;
-var update_time = new Date().getTime();
-var pings = {};
-var ping_rate = 1000;
+var update_time = new Date().getTime()
+var ping_rate = 3000;
 var ping_time = new Date().getTime();
 
 
 // The mighty game loop
 var game_loop = function() {
 
-	// state_older 	<-- state_old
-	// state_old   	<-- players
-	// players  	<-- (new state)
-	update_interpolation_state();
-	update_players();
+	core.updateInterpolationState();
+	core.updatePlayers(tick_length);
+
+	core.spawnedPlayers.forEach(function(v) {
+		send_to_all('s_player_spawned', v);
+	});
 	
-	// Send the updated players
 	send_to_all('s_players', { 
-		players: players,
-		shots: shots,
-		hits: hits,
-		kills: kills,
+		players: core.players,
+		shots: core.shots,
+		hits: core.hits,
+		kills: core.kills,
 		server_time: new Date().getTime()
 	 });
-
-	// Reset
-	shots = [];
-	kills = [];
-	hits = [];
 
 	tick_length = new Date().getTime() - update_time;
 
@@ -92,28 +67,6 @@ var game_loop = function() {
 	setTimeout(game_loop, tick_rate);
 };
 
-var update_players = function() {
-	Object.keys(players).forEach(function(v) {
-
-		if (players[v].timeToSpawn > 0 && players[v].alive === false) {
-			// Player is dead and waiting to spawn
-			players[v].timeToSpawn -= tick_length;
-		} 
-
-		if (players[v].timeToSpawn <= 0 && players[v].alive === false) {
-			// Player has waited long enough
-			core.spawnPlayer(players[v], occluders);
-			console.log('Player', v, 'spawned');
-			send_to_all('s_player_spawned', players[v]);
-		} 
-
-		if (players[v].timeToSpawn <= 0 && players[v].alive === true) {
-			// Player is alive and active
-			apply_delta_queue(v);
-		}
-
-	});	
-};
 
 // Keep an eye on how fast the server is working
 var calculate_average_tick_length = function(last_tick_length) {
@@ -126,74 +79,30 @@ var calculate_average_tick_length = function(last_tick_length) {
 	average_tick_rate /= recent_ticks.length;
 };
 
-// Update interpolation states old and older
-var update_interpolation_state = function() {
-	state_old = {};
-	Object.keys(players).forEach(function(v) {
-		state_old[v] = core.copyForInterpolation(players[v]);
-	});
-};
-
-// Push a delta to the queue for processing next update iteration
-var handle_delta = function(socket_id, delta) {
-	delta_queues[socket_id].push(delta);
-};
-
-// Apply a queue of deltas, reset the queue afterwards
-var apply_delta_queue = function(socket_id) {
-	delta_queues[socket_id].forEach(function(v) {
-		core.applyDelta(players[socket_id], v, players, occluders);
-		handled_deltas[socket_id]++;
-	});
-	delta_queues[socket_id] = [];
-};
 
 // A user has fired. See if someone got hit, using positions from the past.
 var fire = function(socket_id, source, direction) {
-	//console.log('Fire!');
-
-	if (players[socket_id].alive === false) {
-		console.log(players[socket_id].name, 'can\'t shoot because of death! :(');
-		return;
-	}
-
-	shots.push({ shooter: socket_id, source: source });
-	// TODO validate shooter position
-	var hit_data = core.fire(players, state_old, update_time, average_tick_rate, players[socket_id].latency, socket_id, source, direction, occluders);
+	var hit_data = core.fire(update_time, average_tick_rate, socket_id, source, direction);
+	// Send hit confirmation ASAP
 	if (hit_data.target_id > -1) {
-		hits.push( { shooter: socket_id, victim: hit_data.target_id, point: hit_data.point } );
-		console.log(players[hit_data.target_id].name, 'got hit by', players[socket_id].name, '!');
-		if (players[hit_data.target_id].health === 0) {
-
-			players[hit_data.target_id].deaths++;
-			players[socket_id].kills++;
-
-			send_to_all('s_player_killed', { shooter: socket_id, victim: hit_data.target_id });
-			console.log(players[hit_data.target_id].name, 'got killed by', players[socket_id].name, '!');
-			// Add to list of recent kills
-			kills.push( { shooter: socket_id, victim: hit_data.target_id } );
-			// Reset	
-			core.killPlayer(players[hit_data.target_id]);
-		}
-		// Instantly send shot confirmation to the shooter for fast feedback
-		// Don't wait for the next server update, snappy shots is important!
 		send_to_one(socket_id, 's_hit_target', hit_data );
 	}
 };
 
+
 var handle_message = function(socket_id, message, data, seq) {
 	switch (message) {
 		case 'c_pong':
-			players[socket_id].latency = (new Date().getTime() - ping_time)/2;
+			core.setPlayerValue(socket_id, 'latency', (new Date().getTime() - ping_time)/2);
 			break;
 		case 'c_delta':
-			handle_delta(socket_id, data);
+			core.pushDelta(socket_id, data);
 			break;
 		case 'c_fire':
 			fire(socket_id, data.source, data.direction);
 			break;
 		case 'c_name':
-			players[socket_id].name = data;
+			core.setPlayerValue(socket_id, 'name', data);
 			break;
 		default:
 			console.error('Unknown message:', message);
@@ -203,17 +112,14 @@ var handle_message = function(socket_id, message, data, seq) {
 
 
 wss.on('connection', function(ws) {
-	var socket_id;
+	var socket_id, player, init_data;
 
-	num_connections++;
 	socket_id = socket_id_counter++;
 	sockets[socket_id] = ws;
-	players[socket_id] = core.newPlayer(socket_id, occluders);
-	handled_deltas[socket_id] = 0;
-	delta_queues[socket_id] = [];
+	player = core.newPlayer(socket_id);
 
 	console.log('---------------------------------------------------');
-	console.log('New player:', socket_id, players[socket_id].name);
+	console.log('New player:', socket_id, player);
 	console.log('---------------------------------------------------');
 
 	ws.onmessage = function(messageString) {
@@ -235,32 +141,32 @@ wss.on('connection', function(ws) {
 
 	ws.on('close', function() {
 		console.log('---------------------------------------------------');
-		console.log('Disconnected:', socket_id, players[socket_id].name);
+		console.log('Disconnected:', socket_id, core.players[socket_id].name, socket_id);
 		console.log('---------------------------------------------------');
 		delete sockets[socket_id];
-		delete players[socket_id];
-		num_connections--;
+		core.removePlayer(socket_id);
 		send_to_all('s_player_disconnected', socket_id);
 	});
 
 	var init_data = {
-		player: players[socket_id],
-		core: core,
-		occluders: occluders
-	}
+		player: player,
+		constants: core.constants,
+		occluders: core.occluders,
+	};
 
 	send_to_one(socket_id, 's_init', init_data);
-	send_to_all('s_player_connected', players[socket_id]);
+	send_to_all('s_player_connected', player);
 
 });
 
-// Send a message to a specific player
 
+// Send a message to a specific player
 var send_to_one = function(socket_id, message, data) {
 	if (sockets[socket_id] && sockets[socket_id].readyState === 1) {
 		sockets[socket_id].send(JSON.stringify({ socket_id: socket_id, message: message, data: data}));
 	}
 };
+
 
 // Send a message to all players
 var send_to_all = function(message, data) {
@@ -268,6 +174,7 @@ var send_to_all = function(message, data) {
 		send_to_one(v, message, data);
 	});	
 };
+
 
 console.log('Starting game loop');
 game_loop();
